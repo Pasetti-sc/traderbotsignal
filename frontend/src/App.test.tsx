fcae90facf5d2d51709dfb333b666dc9a0f02bd5
Leonardo Castro Pasko <alltvboxnet@gmail.com>
2025-09-01 22:10:42 -0300

    Delete frontend directory

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login page by default', () => {
-  render(<App />);
-  const heading = screen.getByRole('heading', { name: /entrar/i });
-  expect(heading).toBeInTheDocument();
-});
